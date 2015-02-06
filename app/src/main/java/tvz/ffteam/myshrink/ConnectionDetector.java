233f0f19bfbec86272444debfa357587597cde32
Filip Potocki <fpotocki@hotmail.com>
2015-02-06 18:08:05 +0100

    Added ConnectionDetector Class, permissions

---
+++ app/src/main/java/tvz/ffteam/myshrink/ConnectionDetector.java
@@ -0,0 +1,40 @@
+package tvz.ffteam.myshrink;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.*;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Filip on 6.2.2015..
+ */
+public class ConnectionDetector {
+    private Context _context;
+
+    public ConnectionDetector(Context context){
+        this._context = context;
+    }
+
+    public boolean isConnectingToInternet(){
+        ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null)
+        {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                    {
+                        return true;
+                    }
+
+        }
+        return false;
+    }
+  }
